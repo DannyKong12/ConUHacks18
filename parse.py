b74a07a337d227047519e5a5cfc6fd915a8bc9ed
DANNNNYYYYYYYYYY-CISSMATEROV <danny2017313@concordiashanghai.org>
2018-01-28 00:36:19 -0500

    Modified templates

--- parse.py
+++ parse.py
@@ -6,6 +6,11 @@ from PIL import Image
 import pytesseract
 import argparse
 import os
+import subprocess
+
+# exec("ffmpeg -ss -i"+ filename + str(times[i]) + str(120) + outfilename)
+
+
 
 
 templates = [cv.imread("./templates/"+str(i)+".png") for i in range(10)]
@@ -33,62 +38,36 @@ def get_time(m1, m2, s1, s2):
     s2 = np.argmax(s2)
     return (m1, m2, s1, s2)
 
-filepath = "./first_goal.mp4"
+filepath = "./full_match.mp4"
+videoname = "full_match.mp4"
+videoout = "goal1.mp4"
 
 cap = cv.VideoCapture(filepath)
-
 counter = 0
+def clip(start):
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        try:
+            m1 = frame[36:64, 193: 205]
+            m2 = frame[36:64, 204: 217]
+            s1 = frame[36:64, 219: 231]
+            s2 = frame[36:64, 231: 243]
+            current = int(cap.get(0)/1000)
+            time = get_time(m1, m2, s1, s2)
+            start_time = (int(start[0]), int(start[1]), int(start[3]), int(start[4]))
+            a = "00:"+str(np.floor_divide(current, 60)).zfill(2)+":"+str(current%60)
+            print(time, current, start_time, time==start_time)
+            if time == start_time:
+                counter+=1
+                if counter == 5:
+                    subprocess.run(["ffmpeg", "-ss", a, "-i", videoname, "-c", "copy", "-t", "20", videoout])
+                    print("You finally did it you dumbass fuck")
+                    break
+            else:
+                counter = 0
+        except Exception as e:
+            print(e)
+            print("you actual fucking autist jesus christ")
+            break
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    m1 = frame[36:64, 193: 205]
-    m2 = frame[36:64, 204: 217]
-    s1 = frame[36:64, 219: 231]
-    s2 = frame[36:64, 231: 243]
-
-    time = get_time(m1, m2, s1, s2)
-    print("%d%d:%d%d" % time)
-
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    blurred = cv.GaussianBlur(gray, (5, 5), 0)
-    edged = cv.Canny(blurred, 50, 200, 255)
-    inv = cv.subtract(255, edged)
-    cv.imshow('frame', inv)
-
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
-# text = pytesseract.image_to_string(inv)
-# print(text)
-
-#search for the fucking clock here using clock img
-
-
-# image = cv.imread(image)
-# gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-#
-# # check to see if we should apply thresholding to preprocess the
-# # image
-# if args["preprocess"] == "thresh":
-# 	gray = cv.threshold(gray, 0, 255,
-# 		cv.THRESH_BINARY | cv.THRESH_OTSU)[1]
-#
-# # make a check to see if median blurring should be done to remove
-# # noise
-# elif args["preprocess"] == "blur":
-# 	gray = cv.medianBlur(gray, 3)
-#
-# # write the grayscale image to disk as a temporary file so we can
-# # apply OCR to it
-# filename = "{}.png".format(os.getpid())
-# cv.imwrite(filename, gray)
-#
-# # load the image as a PIL/Pillow image, apply OCR, and then delete
-# # the temporary file
-# text = pytesseract.image_to_string(Image.open(filename))
-# os.remove(filename)
-# print(text)
-#
-# # show the output images
-# cv.imshow("Image", image)
-# cv.imshow("Output", gray)
-# cv.waitKey(0)
+clip("08:36")
